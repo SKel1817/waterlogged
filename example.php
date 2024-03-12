8088803df6a19533631d71e9aa70be80fcbeb9a3
SKel1817 <vnielson@oakland.edu>
2024-03-12 17:12:32 -0400

    example of working php

---
+++ example.php
@@ -0,0 +1,28 @@
+<?php
+// Database connection details
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "waterlogged";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM plants";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
