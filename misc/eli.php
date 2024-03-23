4598ca02b51360f85425836bad788ee0a0f39825
Eli Sepulveda <elikartwii@gmail.com>
2024-03-23 14:53:37 -0400

    update stuff

--- misc/eli.php
+++ misc/eli.php
@@ -22,6 +22,17 @@ $sql = "SELECT name, image FROM plants";
 <!-- logs -->
 <?php
 
-$sql = "SELECT * FROM logs WHERE plant_id in (SELECT id FROM user_plants WHERE user_id = $user_id)";
+$sql = "
+SELECT
+    p.name AS PlantName,
+    p.image_url AS PlantImage,
+    l.date_watered AS DateWatered
+FROM
+    logs l
+INNER JOIN user_plants up ON l.user_plant_id = up.id
+INNER JOIN plants p ON up.plant_id = p.id
+ORDER BY
+    l.date_watered DESC;
+";
 
 ?>
--- sql/images_table.sql
+++ sql/images_table.sql
@@ -1,20 +1,29 @@
 CREATE TABLE images (
-    image_id INT PRIMARY KEY AUTO_INCREMENT,
-    plant_id INT,
-    location_id INT,
-    path VARCHAR(255),
-    FOREIGN KEY (plant_id) REFERENCES plants(plant_id),
-    FOREIGN KEY (location_id) REFERENCES locations(location_id)
+    plant_id INT NULL,
+    location_id INT NULL,
+    path VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_plant FOREIGN KEY (plant_id) REFERENCES plants(id),
+    CONSTRAINT fk_location FOREIGN KEY (location_id) REFERENCES locations(id),
+    CONSTRAINT chk_images CHECK ((plant_id IS NOT NULL AND location_id IS NULL) OR (plant_id IS NULL AND location_id IS NOT NULL))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO images (plant_id, location_id, path) VALUES
-(NULL, 1, 'images/1.jpg'),
-(NULL, 2, 'images/2.jpg'),
-(NULL, 3, 'images/3.jpg'),
-(NULL, 4, 'images/4.jpg'),
-(NULL, 5, 'images/5.jpg'),
-(NULL, 6, 'images/6.jpg'),
-(NULL, 7, 'images/7.jpg'),
-(NULL, 8, 'images/8.jpg'),
-(NULL, 9, 'images/9.jpg'),
-(NULL, 10, 'images/10.jpg');
+(NULL, 1, 'imgs/locations/tropical_rainforest.jpg'),
+(NULL, 2, 'imgs/locations/temperate_forest.jpg'),
+(NULL, 3, 'imgs/locations/boreal_forest.jpg'),
+(NULL, 4, 'imgs/locations/tundra.jpg'),
+(NULL, 5, 'imgs/locations/shrubland.jpg'),
+(NULL, 6, 'imgs/locations/desert.jpg'),
+(NULL, 7, 'imgs/locations/grassland.jpg'),
+(NULL, 8, 'imgs/locations/lentic.jpg'),
+(NULL, 9, 'imgs/locations/littoral.jpg'),
+(NULL, 10, 'imgs/locations/lotic.jpg'),
+(NULL, 11, 'imgs/locations/coral_reef.jpg'),
+(NULL, 12, 'imgs/locations/oceanic.jpg');
+
+-- get plant info from plants_data.sql
+INSERT INTO images (plant_id, location_id, path) VALUES
+(1, NULL, 'imgs/plants/philodendron_bipennifolium.jpg'),
+(2, NULL, 'imgs/plants/phalaenopis_spp.jpg'),
+(3, NULL, 'imgs/plants/epipremnum_aureum.jpg'),
+(4, NULL, 'imgs/plants/peperomia_obtusifolia.jpg');
