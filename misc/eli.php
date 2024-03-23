356d84038ab9a0c54c2b905b3259cb3f92d4748b
Eli Sepulveda <elikartwii@gmail.com>
2024-03-23 15:06:46 -0400

    pudate command

--- misc/eli.php
+++ misc/eli.php
@@ -23,14 +23,14 @@ $sql = "SELECT name, image FROM plants";
 <?php
 
 $sql = "
+use waterlogged;
 SELECT
     p.name AS PlantName,
-    p.image_url AS PlantImage,
     l.date_watered AS DateWatered
 FROM
-    logs l
+    waterlogged.logs as l
 INNER JOIN user_plants up ON l.user_plant_id = up.id
-INNER JOIN plants p ON up.plant_id = p.id
+INNER JOIN plants as p ON up.plant_id = p.id
 ORDER BY
     l.date_watered DESC;
 ";
